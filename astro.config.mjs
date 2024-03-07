e46254b8bc8d0e44b34b677e18efae7b0efbee1e
David Rosero Calle <davidroserocalle@gmail.com>
2024-03-07 06:38:00 -0500

    Fix - Eliminando redirect inicial y solucionando media query del cambio de idioma

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
         locales: ['en', 'es'],
         defaultLocale: 'es',
         routing: {
-            prefixDefaultLocale: true
+            prefixDefaultLocale: false
         }
     },
 });
